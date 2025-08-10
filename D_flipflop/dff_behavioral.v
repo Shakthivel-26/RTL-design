2b6089a50905d3dca0712cdd0230401cda334eb7
Shakthivel <shakthivelsachin56@gmail.com>
2025-08-10 21:27:35 +0530

    d flip flop behavioral model

---
+++ D_flipflop/dff_behavioral.v
@@ -0,0 +1,8 @@
+module dff_behavioral(input D, clk, reset, output reg Q);
+  always @(posedge clk or posedge reset) begin
+    if (reset)
+      Q <= 1'b0;
+    else
+      Q <= D;
+  end
+endmodule
